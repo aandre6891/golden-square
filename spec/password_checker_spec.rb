1a0e60d8b03da312cf63860b67a4ce5d73e61608
Andrea Ruggieri <ruggieri6891@gmail.com>
2023-04-12 16:36:37 +0100

    add >8 characters test

---
+++ spec/password_checker_spec.rb
@@ -0,0 +1,14 @@
+# true if password > 8
+# true if password == 8
+# fail if password < 8
+
+require "password_checker.rb"
+
+RSpec.describe "password checker" do
+  context "returns true for password > 8 characters" do
+    it "returns true" do
+      password = PasswordChecker.new
+      expect(password.check("123456789")).to eq(true)
+    end
+  end
+end
