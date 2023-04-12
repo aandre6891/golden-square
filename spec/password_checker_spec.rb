af7687bc95e5d7955c5a3cb10bbdcf0a81950807
Andrea Ruggieri <ruggieri6891@gmail.com>
2023-04-12 16:38:17 +0100

    add test passwords == 8 characters

--- spec/password_checker_spec.rb
+++ spec/password_checker_spec.rb
@@ -11,4 +11,10 @@ RSpec.describe "password checker" do
       expect(password.check("123456789")).to eq(true)
     end
   end
+  context "returns true for password == 8 characters" do
+   it "returns true" do
+      password = PasswordChecker.new
+      expect(password.check("12345678")).to eq(true)
+    end
+  end
 end
