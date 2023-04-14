8fd5517cf3ccd54d6bb7787a458cd64b1982a5de
Andrea Ruggieri <ruggieri6891@gmail.com>
2023-04-14 15:58:39 +0100

    most_common_letter.rb debugged

--- phase2/lib/most_common_letter.rb
+++ phase2/lib/most_common_letter.rb
@@ -1,10 +1,13 @@
 def get_most_common_letter(text)
+  text_with_no_spaces = text.split(" ").join("")
   counter = Hash.new(0)
-  text.chars.each do |char|
+  text_with_no_spaces.chars.each do |char|
     counter[char] += 1
   end
-  counter.to_a.sort_by { |k, v| v }[0][0]
+  puts counter.to_a.sort_by { |k, v| -v }[0][0]
 end
+
+get_most_common_letter("the roof, the roof, the roof is on fire!")
 
 # Intended output:
 # 
