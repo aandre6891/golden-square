d0eee1c2f961fdf97fd07768f9e2e0b57b967c42
Andrea Ruggieri <ruggieri6891@gmail.com>
2023-04-17 10:20:57 +0100

    added test and method count words

--- phase2/spec/diary_entry_spec.rb
+++ phase2/spec/diary_entry_spec.rb
@@ -4,5 +4,13 @@ RSpec.describe "diary entry class" do
   it "constructs" do
     diary_entry = DiaryEntry.new("my_title", "my_contents")
     expect(diary_entry.title).to eq "my_title"
+    expect(diary_entry.contents).to eq "my_contents"
+  end
+
+  describe "#count_words" do
+    it "returns the number of words in the contents" do
+      diary_entry = DiaryEntry.new("my_title", "some contents here")
+      expect(diary_entry.count_words).to eq 3
+    end
   end
 end
