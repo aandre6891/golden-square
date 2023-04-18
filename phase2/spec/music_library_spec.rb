34407bd9ab1a1f93d3409eb33d1f9e9d8560d0c6
Andrea Ruggieri <ruggieri6891@gmail.com>
2023-04-18 16:43:15 +0100

    add first test

--- phase2/spec/music_library_spec.rb
+++ phase2/spec/music_library_spec.rb
@@ -1,5 +1,12 @@
 require "music_library"
 
 RSpec.describe MusicLibrary do
-   
+  context "when we add a track" do
+    it "adds that track to the library" do
+      music_library = MusicLibrary.new
+      track = Track.new("my_title", "my_artist")
+      music_library.add(track)
+      expect(music_library.all).to eq [track]
+    end
+  end
 end
