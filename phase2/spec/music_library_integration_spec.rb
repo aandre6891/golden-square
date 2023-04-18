5ff605502b99e589b298a910742287fa054b580d
Andrea Ruggieri <ruggieri6891@gmail.com>
2023-04-18 17:03:41 +0100

    add test search_by_title

--- phase2/spec/music_library_integration_spec.rb
+++ phase2/spec/music_library_integration_spec.rb
@@ -11,5 +11,26 @@ RSpec.describe "integration" do
     end
   end
 
-  
+  context "when we add multiple tracks to the library" do
+    it "comes back to the list" do
+      music_library = MusicLibrary.new
+      track_1 = Track.new("my_title_1", "my_artist_1")
+      track_2 = Track.new("my_title_2", "my_artist_2")
+      music_library.add(track_1)
+      music_library.add(track_2)
+      expect(music_library.all).to eq [track_1, track_2]
+    end
+  end
+
+  context "with some tracks added" do
+    it "searches for those tracks by full title" do
+      music_library = MusicLibrary.new
+      track_1 = Track.new("my_title_1", "my_artist_1")
+      track_2 = Track.new("my_title_2", "my_artist_2")
+      music_library.add(track_1)
+      music_library.add(track_2)
+      result = music_library.search_by_title("my_title_2")
+      expect(result).to eq [track_2]
+    end
+  end
 end
