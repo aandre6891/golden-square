7609edd1fc82725ddd38e3a1be61883516951214
Andrea Ruggieri <ruggieri6891@gmail.com>
2023-04-20 11:15:57 +0100

    add initially test

---
+++ phase2/spec/multi_todo_list_spec.rb
@@ -0,0 +1,10 @@
+require "multi_todo_list"
+
+RSpec.describe TodoList do
+  context "initially" do
+    it "is empty" do
+      todo_list = TodoList.new
+      expect(todo_list.incomplete).to eq []
+    end
+  end
+end
