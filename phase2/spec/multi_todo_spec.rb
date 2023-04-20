f96acec0dd0fbfea38431f626e76aabfe40e33b8
Andrea Ruggieri <ruggieri6891@gmail.com>
2023-04-20 10:38:33 +0100

    add test #task

--- phase2/spec/multi_todo_spec.rb
+++ phase2/spec/multi_todo_spec.rb
@@ -3,7 +3,7 @@ require "multi_todo"
 RSpec.describe Todo do
   describe "#task" do
     context "given a task" do
-      it "returns the task as a string" do
+      it "returns the task" do
         todo = Todo.new("Wash the car.")
         expect(todo.task).to eq "Wash the car."
       end
