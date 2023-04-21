8f999f90a863cf5fe079fb898186faa2938a70fd
Andrea Ruggieri <ruggieri6891@gmail.com>
2023-04-21 08:47:46 +0100

    add methods to diary_reader

---
+++ phase2/design_multi_class_program/lib/diary_reader.rb
@@ -0,0 +1,12 @@
+class DiaryReader
+  def initialize(wpm, diary) 
+  # diary is an istance of Diary
+  # wpm is a number representing the number of words 
+  # per minute the reader can read
+  end
+
+  def find_most_readable_in_time(time)
+    # returns longest readable DiaryEntry in the given time based on the 
+    # wpm already given 
+  end
+end
