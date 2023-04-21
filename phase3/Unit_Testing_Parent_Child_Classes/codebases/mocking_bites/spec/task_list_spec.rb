b60c37800c5b89b03f38b4a39f72214861976007
Andrea Ruggieri <ruggieri6891@gmail.com>
2023-04-21 17:10:35 +0100

    add mocking test #all

--- phase3/Unit_Testing_Parent_Child_Classes/codebases/mocking_bites/spec/task_list_spec.rb
+++ phase3/Unit_Testing_Parent_Child_Classes/codebases/mocking_bites/spec/task_list_spec.rb
@@ -11,5 +11,15 @@ RSpec.describe TaskList do
     expect(task_list.all_complete?).to eq false
   end
 
+  context "when we add some tasks" do
+    it "returns a list of all the tasks" do
+      task_list = TaskList.new
+      task_1 = double :FakeTask, title: "my task"
+      task_2 = double :FakeTask, title: "my task"
+      task_list.add(task_1)
+      task_list.add(task_2)
+      expect(task_list.all).to eq [task]
+    end
+  end
   # Unit test `#all` and `#all_complete?` behaviour
 end
