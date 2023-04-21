64f475d48476885147054477041e19ef87b0b074
Andrea Ruggieri <ruggieri6891@gmail.com>
2023-04-21 10:58:51 +0100

    add test and file phonenumbercrawler

---
+++ phase2/design_multi_class_program/lib/phone_number_crawler.rb
@@ -0,0 +1,10 @@
+class PhoneNumberCrawler
+  def initialize(diary) # diary is an istance of Diary
+  
+  end
+
+  def extract_numbers
+    # gets numbers from the diary entries
+    # and returns a list of numbers as strings
+  end
+end
--- phase2/design_multi_class_program/spec/diary_integration_spec.rb
+++ phase2/design_multi_class_program/spec/diary_integration_spec.rb
@@ -1,6 +1,7 @@
 require "diary"
 require "diary_entry"
 require "diary_reader"
+require "phone_number_crawler"
 
 RSpec.describe "diary integration" do
   context "given more DiaryEntries" do
@@ -48,8 +49,7 @@ RSpec.describe "diary integration" do
         expect(diary_reader.find_most_readable_in_time(2)).to eq diary_entry_3
       end
     end
-  end
-
+    
     context "when there is nothing readable in the given time" do
       it "returns nil" do
         diary = Diary.new
@@ -59,7 +59,7 @@ RSpec.describe "diary integration" do
         expect(diary_reader.find_most_readable_in_time(2)).to eq nil
       end
     end
-
+    
     context "when there is nothing at all in the Diary" do
       it "should return nil" do
         diary = Diary.new
@@ -75,12 +75,29 @@ RSpec.describe "diary integration" do
         expect { diary_reader.find_most_readable_in_time(2) }.to raise_error "wpm should be positive"
       end
     end
-
-# # 6 - wpm is zero
-# diary = Diary.new
-# diary_reader = DiaryReader.new(2, diary)
-# expect(diary_reader.find_most_readable_in_time(2)).to eq nil
-
+  end
+    
+  describe "phone number extraction behaviour" do
+    it "extracts the numbers from all the  diary entries" do
+      diary = Diary.new
+      phone_book = PhoneNumberCrawler.new(diary)
+      diary_entry_0 = DiaryEntry.new("title0", "my friend is cool")
+      diary_entry_1 = DiaryEntry.new("title1", "my friend's number is 123456789")
+      diary_entry_2 = DiaryEntry.new("title2", "my friend's numbers are 123456789, 987654321. 918273645 and 918273645")
+      diary.add(diary_entry_0)
+      diary.add(diary_entry_1)
+      diary.add(diary_entry_2)
+      result = phone_book.extract_numbers
+      expect(result).to eq ["123456789", "987654321", "918273645"]
+    end
+  end
+  
+    xit "doesn't extract invalid numbers"
+      diary = Diary.new
+      phone_book = PhoneNumberCrawler.new(diary)
+      diary_entry_3 = DiaryEntry.new("title3", "")
+      diary.add(diary_entry_3)
+end
 
 # # 7
 # task_list = TaskList.new
@@ -89,16 +106,3 @@ RSpec.describe "diary integration" do
 # task_list.add(task_1)
 # task_list.add(task_2)
 # expect(task_list.all).to eq [task_1, task_2] 
-
-
-# # 8
-# diary = Diary.new
-# diary_entry_1 = DiaryEntry.new("title1", "my friend's number is 123456789")
-# diary_entry_2 = DiaryEntry.new("title2", "my friend's numbers are 123456789, 987654321. 918273645 and 918273645")
-# diary.add(diary_entry_1)
-# diary.add(diary_entry_ 2)
-# phone_book = PhoneNumberCrawler.new(diary)
-# result = phone_book.extract_numbers
-# expect(result)).to eq ["123456789", "987654321", "918273645"]
-
-end
