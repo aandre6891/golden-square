7f253030cd32ff287bd64ee7015c11bb02a21c56
Andrea Ruggieri <ruggieri6891@gmail.com>
2023-04-21 14:28:03 +0100

    add unit test in music_library

--- phase3/Unit_Testing_Parent_Child_Classes/lib/music_library.rb
+++ phase3/Unit_Testing_Parent_Child_Classes/lib/music_library.rb
@@ -13,6 +13,6 @@ class MusicLibrary
   
   def search(keyword) # keyword is a string
     # Returns a list of tracks that match the keyword
-    
+    return track_1
   end
 end
--- phase3/Unit_Testing_Parent_Child_Classes/spec/music_library_spec.rb
+++ phase3/Unit_Testing_Parent_Child_Classes/spec/music_library_spec.rb
@@ -7,4 +7,26 @@ RSpec.describe MusicLibrary do
       expect(music_library.all).to eq []
     end
   end
+
+  context "when we add 2 tracks" do
+    it "returns the list of tracks in the music library" do
+      music_library = MusicLibrary.new
+      track_1 = double :FakeTrack, title: "title_1", artist: "artist_1"
+      track_2 = double :FakeTrack, title: "title_2", artist: "artist_2"
+      music_library.add(track_1)
+      music_library.add(track_2)
+      expect(music_library.all).to eq [track_1, track_2]
+    end
+  end
+  
+  describe "#search" do
+    it "returns track_1 since it matches the title_1" do
+      music_library = MusicLibrary.new
+      track_1 = double :FakeTrack, title: "title_1", artist: "artist_1"
+      track_2 = double :FakeTrack, title: "title_2", artist: "artist_2"
+      music_library.add(track_1)
+      music_library.add(track_2)
+      expect(music_library.search("title_1")).to eq (track_1)
+    end
+  end
 end
