b2f3c0d298945b10269274ffb162a727896c58d4
Andrea Ruggieri <ruggieri6891@gmail.com>
2023-04-22 09:34:37 +0100

    add mocking tests #all_complete?

--- phase3/Unit_Testing_Parent_Child_Classes/codebases/mocking_bites/spec/task_list_spec.rb
+++ phase3/Unit_Testing_Parent_Child_Classes/codebases/mocking_bites/spec/task_list_spec.rb
@@ -29,5 +29,31 @@ RSpec.describe TaskList do
         expect(task_list.all_complete?).to eq false
       end
     end
+
+    context "when all the tasks are complete" do
+      it "it returns true" do
+        task_list = TaskList.new
+        task_1 = double :FakeTask, title: "my task"
+        expect(task_1).to receive(:complete?).and_return(true)
+        task_list.add(task_1)
+        expect(task_list.all_complete?).to eq true
+      end
+    end
+
+    context "when there are uncomplete tasks" do
+      it "it returns false" do
+        task_list = TaskList.new
+        task_1 = double :FakeTask, title: "my task 1"
+        expect(task_1).to receive(:complete?).and_return(true)
+        task_2 = double :FakeTask, title: "my task 2"
+        expect(task_2).to receive(:complete?).and_return(true)
+        task_3 = double :FakeTask, title: "my task 3"
+        expect(task_3).to receive(:complete?).and_return(false)
+        task_list.add(task_1)
+        task_list.add(task_2)
+        task_list.add(task_3)
+        expect(task_list.all_complete?).to eq false
+      end
+    end
   end
 end
