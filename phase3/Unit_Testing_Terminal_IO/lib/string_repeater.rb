63f8ad473ad3d23fb3b752b9b9b5dc326307a196
Andrea Ruggieri <ruggieri6891@gmail.com>
2023-04-23 15:11:13 +0100

    add tests for string or number of repeats not valid

--- phase3/Unit_Testing_Terminal_IO/lib/string_repeater.rb
+++ phase3/Unit_Testing_Terminal_IO/lib/string_repeater.rb
@@ -14,15 +14,22 @@ class StringRepeater
   def repeater
     @terminal.puts "Hello. I will repeat a string many times."
     @terminal.puts "Please enter a string"
-    string = @terminal.gets
+    string = get_string
     @terminal.puts "Please enter a number of repeats"
     number_of_repeats = get_number_of_repeats
     @terminal.puts "Here is your result:"
-    @terminal.puts "#{string * 10}"
+    @terminal.puts "#{string * number_of_repeats}"
+  end
+
+  def get_string
+    user_string = @terminal.gets
+    fail "empty string not accepted" if user_string == ""
+    return user_string.to_s
   end
 
   def get_number_of_repeats
     user_input = @terminal.gets
+    fail "number of repeats cannot be empty" if user_input == ""
     fail "not a valid number of repeats" unless user_input == user_input.to_i.to_s
     return user_input.to_i
   end
--- phase3/Unit_Testing_Terminal_IO/spec/string_repeater_spec.rb
+++ phase3/Unit_Testing_Terminal_IO/spec/string_repeater_spec.rb
@@ -13,6 +13,7 @@ RSpec.describe StringRepeater do
     expect(terminal).to receive(:gets).and_return("10").ordered
     expect(terminal).to receive(:puts).with("Here is your result:").ordered
     expect(terminal).to receive(:puts).with("TWIXTWIXTWIXTWIXTWIXTWIXTWIXTWIXTWIXTWIX").ordered
+
     string_repeater = StringRepeater.new(terminal)
     string_repeater.repeater
   end
@@ -24,7 +25,30 @@ RSpec.describe StringRepeater do
     expect(terminal).to receive(:gets).and_return("TWIX").ordered
     expect(terminal).to receive(:puts).with("Please enter a number of repeats").ordered
     expect(terminal).to receive(:gets).and_return("a string instead of an integer").ordered
+
     string_repeater = StringRepeater.new(terminal)
     expect{ string_repeater.repeater }.to raise_error("not a valid number of repeats")
+  end
+
+  it "it returns fail if string is empty" do
+    terminal = double(:terminal)
+    expect(terminal).to receive(:puts).with("Hello. I will repeat a string many times.").ordered
+    expect(terminal).to receive(:puts).with("Please enter a string").ordered
+    expect(terminal).to receive(:gets).and_return("").ordered
+
+    string_repeater = StringRepeater.new(terminal)
+    expect{ string_repeater.repeater }.to raise_error("empty string not accepted")
+  end
+
+  it "it returns fail if no number of repeats is given" do
+    terminal = double(:terminal)
+    expect(terminal).to receive(:puts).with("Hello. I will repeat a string many times.").ordered
+    expect(terminal).to receive(:puts).with("Please enter a string").ordered
+    expect(terminal).to receive(:gets).and_return("TWIX").ordered
+    expect(terminal).to receive(:puts).with("Please enter a number of repeats").ordered
+    expect(terminal).to receive(:gets).and_return("").ordered
+
+    string_repeater = StringRepeater.new(terminal)
+    expect{ string_repeater.repeater }.to raise_error("number of repeats cannot be empty")
   end
 end
